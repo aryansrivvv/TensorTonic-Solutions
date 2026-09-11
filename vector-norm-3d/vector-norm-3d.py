b168a779f5bfec1e28d2b0176016bae920f6b01b
Aryan Srivastava <aryansrivv@gmail.com>
2026-09-12 02:36:46 +0530

    Solve vector-norm-3d

--- vector-norm-3d/vector-norm-3d.py
+++ vector-norm-3d/vector-norm-3d.py
@@ -4,7 +4,10 @@ def vector_norm_3d(v: list) -> float | np.ndarray:
     """
     Returns a float or a NumPy array.
     """
-    v = np.asarray(v,dtype = float)
-    x = np.sum(v**2,axis = -1)
-    y = np.sqrt(x,dtype = float)
-    return y 
+    # v = np.asarray(v,dtype = float)
+    # x = np.sum(v**2,axis = -1)
+    # y = np.sqrt(x,dtype = float)
+    # return y 
+    values = np.asarray(v, dtype=float)
+    norms = np.sqrt(np.sum(values ** 2, axis=-1))
+    return float(norms) if norms.ndim == 0 else norms
